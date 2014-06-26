a160234318887396647ae35a73d85322275cec14
Nadeem Mohammad <coolmind182006@gmail.com>
2014-06-26 22:03:11 +0530

    mavenized

---
+++ src/main/java/com/nadeem/app/ocr/TesseractExample.java
@@ -0,0 +1,22 @@
+package com.nadeem.app.ocr;
+
+import java.io.File;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+public class TesseractExample
+{
+    public static void main(String[] args) {
+        File imageFile = new File("eurotext.pdf");
+        Tesseract instance = Tesseract.getInstance(); // JNA Interface Mapping
+        // Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
+
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
